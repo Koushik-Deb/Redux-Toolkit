c9dfb4786375bb150eb9a0e8a7e038693d6e318c
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-03-23 12:05:51 +0600

    Add redux devtools with trace mode enabled

--- redux-starter/src/Store/configureStore.js
+++ redux-starter/src/Store/configureStore.js
@@ -1,7 +1,19 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import taskReducer from "./task";
 
-const store = createStore(taskReducer, applyMiddleware(thunk));
+const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            trace: true,
+            traceLimit: 25
+        })
+        : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+);
+const store = createStore(taskReducer, enhancer);
 
 export default store;
