991352b19762d0445888cad7793ed315802c9366
Koushik Deb <koushik.deb@tigerit.com>
2024-03-24 11:18:56 +0600

    Add createSlice with all functionalities

--- redux-starter/src/Store/task.js
+++ redux-starter/src/Store/task.js
@@ -1,43 +1,79 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Action Types
-const ADD_TASK = "ADD_TASK";
-const REMOVE_TASK = "REMOVE_TASK";
-const TASK_COMPLETED = "TASK_COMPLETED";
-
-// Actions
-export const addTask = createAction(ADD_TASK);
-export const removeTask = createAction(REMOVE_TASK);
-export const completeTask = createAction(TASK_COMPLETED);
-export const fetchTodos = () => async (dispatch, getState) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const data = await response.json();
-  dispatch(addTask(data.title));
-};
-
-// Reducer
+// Reducer // Create Slice
 let id = 0;
 
-export default createReducer([], (builder) => {
-  builder.addCase(ADD_TASK, (state, action) => {
-    state.push({
-      id: ++id,
-      task: action.payload,
-      completed: false,
-    });
-  });
-  builder.addCase(REMOVE_TASK, (state, action) => {
-    return state.filter((todo) => todo.id !== action.payload);
-  });
-  builder.addCase(TASK_COMPLETED, (state, action) => {
-    return state.map((todo) => {
-      if (todo.id === action.payload) {
-        return {
-          ...todo,
-          completed: true,
-        };
-      }
-      return todo;
-    });
-  });
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload,
+        completed: false,
+      });
+    },
+    removeTask: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    completeTask: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: true,
+          };
+        }
+        return todo;
+      });
+    },
+  },
 });
+
+console.log("taskSlice", taskSlice);
+export const { addTask, removeTask, completeTask } = taskSlice.actions;
+export default taskSlice.reducer;
+
+// Action Types
+// const ADD_TASK = "ADD_TASK";
+// const REMOVE_TASK = "REMOVE_TASK";
+// const TASK_COMPLETED = "TASK_COMPLETED";
+
+// Actions
+// export const addTask = createAction(ADD_TASK);
+// export const removeTask = createAction(REMOVE_TASK);
+// export const completeTask = createAction(TASK_COMPLETED);
+// export const fetchTodos = () => async (dispatch, getState) => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   const data = await response.json();
+//   dispatch(addTask(data.title));
+// };
+
+// Reducer
+
+// let id = 0;
+
+// export default createReducer([], (builder) => {
+//   builder.addCase(ADD_TASK, (state, action) => {
+//     state.push({
+//       id: ++id,
+//       task: action.payload,
+//       completed: false,
+//     });
+//   });
+//   builder.addCase(REMOVE_TASK, (state, action) => {
+//     return state.filter((todo) => todo.id !== action.payload);
+//   });
+//   builder.addCase(TASK_COMPLETED, (state, action) => {
+//     return state.map((todo) => {
+//       if (todo.id === action.payload) {
+//         return {
+//           ...todo,
+//           completed: true,
+//         };
+//       }
+//       return todo;
+//     });
+//   });
+// });
